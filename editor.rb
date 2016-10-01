a9a1a1b1d65981f2ca6fd1434f12f14efe920d24
Bryan Mytko <bryanmytko@gmail.com>
2016-10-01 19:48:51 -0400

    Add ctrl-c to quit

--- editor.rb
+++ editor.rb
@@ -1,21 +1,28 @@
 #!/usr/bin/env ruby
 
+require "io/console"
+require_relative "screen"
+
 class Editor
   def main
     Screen.with_screen do |screen|
       loop do
         char = read_char
-        render(char)
+        if char == ?\C-c
+          raise SystemExit
+        end
+
+        render(char.inspect)
       end
     end
   end
 
   def read_char
-    $stdin.readChar
+    $stdin.readchar
   end
 
   def render(char)
-    puts char
+    $stdout.write char
   end
 end
 
